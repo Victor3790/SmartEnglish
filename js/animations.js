450e288e225630b66ca5969f50dca25aea8aabd0
victor <victor182@msn.com>
2019-02-05 14:31:11 -0600

    Final version of the web page renewed

--- js/animations.js
+++ js/animations.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
   var zeroScroll = $('#hero').outerHeight();
   var topButton = $('#top');
+  var extraScroll = $('#fixed').outerHeight();
+  var mapScreen = $('.fullScreenMap');
 
   if($(window).width() <= 575){
     var menuLinks = $("a[id*='Small']");
-    var extraScroll = $('#fixed').outerHeight() - $('.smallMenu').outerHeight();
   }else{
     var menuLinks = $("a[id*='Link']");
-    var extraScroll = $('#fixed').outerHeight();
   }
 
   //Responsive menu
@@ -16,13 +16,16 @@ $(document).ready(function(){
     var smallMenu = $('.smallMenu');
 
     if(smallMenu.css('width') === '0px'){
+      smallMenu.css('height','100vh');
       smallMenu.animate({
         width: '40%'
       }, 150);
     }else{
       smallMenu.animate({
         width: '0'
-      }, 150);
+      }, 150, function(){
+        smallMenu.css('height','0');
+      });
     }
   });
 
@@ -65,6 +68,41 @@ $(document).ready(function(){
       }
     });
 
+  });
+
+  //Initialize the map
+  function initMap() {
+    var SmartEnglish = {lat: 19.507379, lng: -98.881107};
+    var map = new google.maps.Map(document.getElementById('showMap'), {
+      center: SmartEnglish,
+      zoom: 18
+    });
+
+    var marker = new google.maps.Marker({
+      position: SmartEnglish,
+      map: map,
+      label: 'Smart English Texcoco.',
+      draggable: true,
+      animation: google.maps.Animation.DROP
+    });
+  }
+
+  //Show the map
+  $('#mapButton').click(function(){
+    initMap();
+    mapScreen.animate({
+      height: '100vh'
+    },1000, function(){
+      $('#close').show('slow');
+    });
+  });
+
+  //Close the map
+  $('#close').click(function(){
+    $('#close').hide('slow');
+    mapScreen.animate({
+      height: 0
+    }, 1000);
   });
 
 });
